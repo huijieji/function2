0321b41079ec30ddf50d4d32c951cb86790dedf5
huijieji <jih191@newschool.edu>
2025-04-09 20:25:29 -0400

    // Function to render your items

--- assets/healing.js
+++ assets/healing.js
@@ -1,1 +1,15 @@
-// 
+// Function to render your items
+const releaseBtn = document.getElementById('releaseBtn');
+const comfortBtn = document.getElementById('comfortBtn');
+const heartbreakMessage = document.getElementById('heartbreak-message');
+const entryList = document.getElementById('entry-list');
+const noEntries = document.getElementById('noEntries');
+const saveModal = document.getElementById('saveModal');
+const quoteModal = document.getElementById('quoteModal');
+const musicSection = document.getElementById('music-section');
+const modalQuote = document.getElementById('modalQuote');
+const quoteAuthor = document.getElementById('quoteAuthor');
+const nextQuoteBtn = document.getElementById('nextQuoteBtn');
+const spotifyPlayer = document.getElementById('spotify-player');
+const quoteLoading = document.getElementById('quote-loading');
+const mainQuote = document.getElementById('main-quote');
