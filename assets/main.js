18645896a762dd7de78dcf3ef30c0d6386ef8421
huijieji <jih191@newschool.edu>
2025-04-15 18:34:35 -0400

    main container  and name update

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-// This is a heartbreak healing website I built to help people cope with breakups
+// This is a Healing Space for the Heartbroken website I built to help people cope with breakups
 // Core features: mood journaling, therapeutic music, and inspirational quotes
 // This project is part of my ongoing exploration of JavaScript and emotional web design. 
 // The idea is to help people going through breakups express their feelings and find comfort in quotes and get music suggestions that match their mood. 
